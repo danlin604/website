a402feecc56deb28a2730ed370d32c943bd3b9bb
Daniel Lin <danlin604@gmail.com>
2017-07-17 20:27:41 -0700

    Added contact tab and function to reduce spam.
    Signed-off-by: Daniel Lin <danlin604@gmail.com>

--- scripts/nav.js
+++ scripts/nav.js
@@ -10,7 +10,6 @@ function expandMenu() {
 }
 
 function loadComponent(component) {
-    console.log(component)
     let main = document.getElementById('main')
     
     // clear innerHTML
@@ -20,4 +19,31 @@ function loadComponent(component) {
 
     let element = document.createElement(component)
     main.appendChild(element)
+}
+
+/* Contact Email - Reduce Email Spam */
+const decode = function(str) {
+    str = str.replace(/[a-z]/gi, function(char) {
+        return String.fromCharCode(char.charCodeAt() - 1)
+    })
+    return str
+}
+
+const generateEmailList = function(str) {
+    let list = []
+    for(let i = 0; i < 16; i++) {
+        str = decode(str)
+        list.push(str + '@gmail.com')
+    }
+    list.push('mailto:')
+    return list
+}
+
+const insertEmail = function(str) {
+    let list = generateEmailList(str)    
+    let contact = document.getElementById('contact')
+    contact.innerText = list[3]
+    contact.onclick = function() {
+        window.location.href = list[16] + list[3]
+    }
 }
